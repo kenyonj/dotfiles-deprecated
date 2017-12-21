f995cd64cc77b3c69ef86e59b3a9b70d52822b37
Justin Kenyon <kenyonj@gmail.com>
2017-12-21 00:25:35 -0500

    more zsh prompt changes

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,52 +1,69 @@
 setopt promptsubst
 
-_current_branch() {
-  echo $(git rev-parse --abbrev-ref HEAD)
+_git_branch(){
+  __ref=$(\git symbolic-ref HEAD 2> /dev/null) || __ref="detached" || return;
+  echo -n "${__ref#refs/heads/}";
+  unset __ref;
 }
 
-_git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null)
-  if [ -n $ref ]; then
-    branch_name="${ref#refs/heads/}"
-    branch_name_max_length=$(($COLUMNS/5))
-    if [ ${#branch_name} -gt $branch_name_max_length ]; then
-      echo "$branch_name[0,$(($branch_name_max_length-3))]..."
-    else
-      echo $branch_name
-    fi
+_git_rev(){
+  __rev=$(\git rev-parse HEAD | cut -c 1-7);
+  echo -n "${__rev}";
+  unset __rev;
+}
+
+_git_remote_defined(){
+  if [ ! -z "`\git remote -v | head -1 | awk '{print $1}' | tr -d ' \n'`" ]; then
+    echo -ne 1
+  else
+    echo -ne 0
   fi
 }
 
-_git_status() {
-  if $(echo "$(git log origin/$(_current_branch)..HEAD 2> /dev/null)" | grep '^commit' &> /dev/null); then
-    echo "changed"
-  elif $(echo "$INDEX" | grep -E -e '^(D[ M]|[MARC][ MD]) ' &> /dev/null); then
-    echo "pending"
-  elif $(echo "$INDEX" | grep -E -e '^(A[AU]|D[DU]|U[ADU]) ' &> /dev/null); then
-    echo "pending"
-  elif $(echo "$INDEX" | grep -E -e '^[ MARC][MD] ' &> /dev/null); then
-    echo "untracked"
-  elif $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
-    echo "untracked"
+_git_remote_name(){
+  if \git remote -v | grep origin > /dev/null; then
+    echo -ne "origin"
   else
-    echo "unchanged"
+    echo -ne "`\git remote -v | head -1 | awk '{print $1}' | tr -d " \n"`"
   fi
 }
 
-_git_prompt_color() {
-  if [ -n "$1" ]; then
-    current_git_status=$(_git_status)
-    if [ "changed" = $current_git_status ]; then
-      echo "$(_red $1)"
-    elif [ "pending" = $current_git_status ]; then
-      echo "$(_yellow $1)"
-    elif [ "unchanged" = $current_git_status ]; then
-      echo "$(_green $1)"
-    elif [ "untracked" = $current_git_status ]; then
-      echo "$(_cyan $1)"
-    fi
+_git_dirty(){
+  GIT_TRACKED_COLOR=cyan
+  GIT_UNTRACKED_COLOR=red
+  GIT_UNPUSHED_COLOR=yellow
+
+  __mod_t=$(\git status --porcelain 2>/dev/null | grep '^M[A,M,D,R, ]\{1\} \|^R[A,M,D,R, ]\{1\} ' | wc -l | tr -d ' ');
+  __add_t=$(\git status --porcelain 2>/dev/null | grep '^A[A,M,D,R, ]\{1\} ' | wc -l | tr -d ' ');
+  __del_t=$(\git status --porcelain 2>/dev/null | grep '^D[A,M,D,R, ]\{1\} ' | wc -l | tr -d ' ');
+  
+  __mod_ut=$(\git status --porcelain 2>/dev/null | grep '^[A,M,D,R, ]\{1\}M \|^[A,M,D,R, ]\{1\}R ' | wc -l | tr -d ' ');
+  __add_ut=$(\git status --porcelain 2>/dev/null | grep '^[A,M,D,R, ]\{1\}A ' | wc -l | tr -d ' ');
+  __del_ut=$(\git status --porcelain 2>/dev/null | grep '^[A,M,D,R, ]\{1\}D ' | wc -l | tr -d ' ');
+  
+  __new=$(\git status --porcelain 2>/dev/null | grep '^?? ' | wc -l | tr -d ' ');
+  __unpushed=$(git rev-list $(_git_branch)..$(_git_remote_name)/$(_git_branch) --count 2>/dev/null);
+  __unpushed_other_way=$(git rev-list $(_git_remote_name)/$(_git_branch)..$(_git_branch) --count 2>/dev/null);
+
+
+  [[ "$__add_t" != "0" ]]              && echo -n " %B%F{$GIT_TRACKED_COLOR}$(_git_branch)%b%f";
+  [[ "$__add_ut" != "0" ]]             && echo -n " %B%F{$GIT_UNTRACKED_COLOR}$(_git_branch)%b%f";
+  [[ "$__mod_t" != "0" ]]              && echo -n " %B%F{$GIT_TRACKED_COLOR}$(_git_branch)%b%f";
+  [[ "$__mod_ut" != "0" ]]             && echo -n " %B%F{$GIT_UNTRACKED_COLOR}$(_git_branch)%b%f";
+  [[ "$__del_t" != "0" ]]              && echo -n " %B%F{$GIT_TRACKED_COLOR}$(_git_branch)%b%f";
+  [[ "$__del_ut" != "0" ]]             && echo -n " %B%F{$GIT_UNTRACKED_COLOR}$(_git_branch)%b%f";
+  [[ "$__new" != "0" ]]                && echo -n " %B%F{$GIT_UNTRACKED_COLOR}$(_git_branch)%b%f";
+  [[ "$__unpushed" != "0" ]]           && echo -n " %B%F{$GIT_UNPUSHED_COLOR}$(_git_branch)%b%f";
+  [[ "$__unpushed_other_way" != "0" ]] && echo -n " %B%F{$GIT_UNPUSHED_COLOR}$(_git_branch)%b%f";
+
+  unset __mod_ut __add_ut __mod_t __new __add_t __del_t __del_ut __unpushed
+}
+
+_git_clean_or_dirty() {
+  if [ -z "$(_git_dirty)" ]; then
+    echo -n " %B%F{green}$(_git_branch)%b%f"
   else
-    echo "$1"
+    echo $(_git_dirty)
   fi
 }
 
@@ -65,21 +82,12 @@ _cyan()                   { echo "$(_color "$1" cyan)" }
 _blue()                   { echo "$(_color "$1" blue)" }
 
 _working_directory()      { echo "$(_blue "%c")" }
-_colored_git_branch()     { echo "$(_git_prompt_color "$(_git_prompt_info)")" }
 
 _display_current_vim_mode() {
   if [[ $VIMODE == 'vicmd' ]]; then
     echo "$(_red "✘")"
   else
     echo "$(_green "✔")"
-  fi
-}
-
-_set_cursor_shape() {
-  if [[ $VIMODE == 'vicmd' ]]; then
-    echo -e -n "\x1b[\x32 q"
-  else
-    echo -e -n "\x1b[\x33 q"
   fi
 }
 
@@ -91,5 +99,5 @@ function zle-line-init zle-keymap-select {
 zle -N zle-line-init
 zle -N zle-keymap-select
 
-PROMPT='$(_working_directory)$(_separate $(_colored_git_branch)) $(_display_current_vim_mode) '
+PROMPT='$(_working_directory)$(_separate $(_git_clean_or_dirty)) $(_display_current_vim_mode)  '
 
